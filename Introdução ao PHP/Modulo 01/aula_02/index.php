eb8efb7c0fcdad1e08492552a6d625f14f6c20bb
hochiminh1996 <ambrsp@hotmail.com>
2023-09-04 22:48:22 -0300

    introdução ao php

---
+++ Introdução ao PHP/Modulo 01/aula_02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Informações do PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="#">
+</head>
+
+<body>
+    <?php 
+        phpinfo();
+    ?>
+
+</body>
+
+</html>

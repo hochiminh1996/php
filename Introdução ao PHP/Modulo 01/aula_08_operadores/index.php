bb6f014523f761cace953c7df194613e73b8618f
hochiminh1996 <ambrsp@hotmail.com>
2023-09-07 16:04:34 -0300

    ordem de precedência dos operadores : **, +, -, / e ()

---
+++ Introdução ao PHP/Modulo 01/aula_08_operadores/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+<body>
+    <?php 
+        /*
+            Expressões Aritméticas
+
+            Precedência de operadores.
+            1 (**) potência
+            2 (*, /, %)
+            3 (+, -)
+
+            Observação: Os parênteses são os modificadores da ordem de precedência dos operadores aritméticos.
+        
+        */
+        echo 50/(2+3)**2;
+        
+    
+    
+    ?>
+</body>
+</html>

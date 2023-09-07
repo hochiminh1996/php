ca5fa8d9fe80a4a82122b5d24dc11e025abcc333
hochiminh1996 <ambrsp@hotmail.com>
2023-09-07 15:18:51 -0300

    analisando dados de entrada e usando o operador de coalescência em php(??)

--- Introdução ao PHP/Modulo 01/aula_07_dados_formulario/php/cadastro.php
+++ Introdução ao PHP/Modulo 01/aula_07_dados_formulario/php/cadastro.php
@@ -1,13 +1,36 @@
-<?php 
-    $nome = $_GET['nome'];
-    $sobrenome = $_GET['sobrenome'];
-    
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    echo "Olá, $nome $sobrenome. Hoje é ".date("d/m/Y");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processando...</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
 
+<body>
+    <header id="cabecalho">
+        <h1>Resultado</h1>
+    </header>
 
+    <main id="principal">
 
+        <section id="conteudo">
+            <?php
+            $nome = $_GET['nome'] ?? "NULL";
+            $sobrenome = $_GET['sobrenome'] ??"NULL";
+            // $_GET[]; é uma superglobal, assim como $_GET[] e $_REQUEST
 
+            if($nome !=null and $sobrenome!=null){
+                echo "<p>Olá, $nome $sobrenome. Bem-vindo. Hoje é ".date("d/m/Y");
+            }else{
+                echo "<p>Erro</p>";
+            }
+            
+            ?>
+            <a href="javascript:history.go(-1)">Voltar</a>
+        </section>
+    </main>
+</body>
 
-    
-?>
+</html>

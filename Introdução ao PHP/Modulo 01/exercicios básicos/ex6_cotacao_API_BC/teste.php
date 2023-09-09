c2b93155b419f77bc917278394f0ec7ec2eb843b
hochiminh1996 <ambrsp@hotmail.com>
2023-09-09 18:15:26 -0300

    correções'

--- Introdução ao PHP/Modulo 01/exercicios básicos/ex6_cotacao_API_BC/teste.php
+++ Introdução ao PHP/Modulo 01/exercicios básicos/ex6_cotacao_API_BC/teste.php
@@ -1,24 +1,26 @@
-<?php 
-    $entrada = 10;
+<?php
+
+$padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
     $inicio = date("m-d-Y", strtotime("-7 days")); //a data de hoje - 7 dias antes 
 
-    $fim = date("m-d-Y"); //pega a data atual do sistema.
+$fim = date("m-d-Y"); //pega a data atual do sistema.
 
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+$url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
-    // Está em aspas simples porque, no php, as aspas duplas fazem interpolação e interpretam os $. No caso, a interpolação pode tentar processar os $ que não são variáveis. Quando as aspas simples na URL, é necessário colocar o caractere de escape ( \ ) antes e depois das ''.
+// Está em aspas simples porque, no php, as aspas duplas fazem interpolação e interpretam os $. No caso, a interpolação pode tentar processar os $ que não são variáveis. Quando as aspas simples na URL, é necessário colocar o caractere de escape ( \ ) antes e depois das ''.
 
-    $dados = json_decode(file_get_contents($url), true);
-    // true => você pega os dados na estrutura de dados de um array. Se for false, vem na estrutura de dados de objeto.
+$dados = json_decode(file_get_contents($url), true);
+// true => você pega os dados na estrutura de dados de um array. Se for false, vem na estrutura de dados de objeto.
 
-    // var_dump($dados);
+// var_dump($dados);
 
-    $cotacao = $dados['value'][0]['cotacaoCompra'];
-    //acessando os dados 
+$cotacao = $dados['value'][0]['cotacaoCompra'];
+//acessando os dados 
 
-    //
+//
 
-    echo sprintf("A cotação é %.2f. Seu valor em R$ $entrada equivale a %.2f", $cotacao, $entrada,  $entrada/$cotacao);
-    
+var_dump($dados);
+// echo sprintf("A cotação é %.2f. Seu valor em R$ $entrada equivale a %.2f", $cotacao, $entrada,  $entrada / $cotacao);
 
-?>
+echo "Cotação: ".numfmt_format_currency($padrao, $cotacao, "USD");
